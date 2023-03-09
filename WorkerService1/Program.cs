cbd2c6a7c0f25807ba9279d7f61c285bb4e8299a
Oleg Azemsha <bguproga@gmail.com>
2023-03-09 17:56:57 +0300

    fix

--- WorkerService1/Program.cs
+++ WorkerService1/Program.cs
@@ -1,11 +1,13 @@
 using WorkerService1;
+using System.Net;
+using System.Net.Sockets;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Generator>();
         services.AddHostedService<Listener>();
-        services.AddSingleton<UdpServer>(provider => new UdpServer("127.0.0.1", 22220));
+        services.AddSingleton<UdpClient>(provider => new UdpClient("127.0.0.1", 22221));
     })
     .Build();
 
---
+++ WorkerService1/Workers.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WorkerService1
+{
+    public class Generator : BackgroundService
+    {
+        private readonly ILogger<Generator> _logger;
+        private readonly UdpClient _server;
+
+        public Generator(ILogger<Generator> logger, UdpClient server)
+        {
+            _logger = logger;
+            _server = server;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                var message = DateTimeOffset.Now.ToString();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                await _server.SendAsync(data);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+
+    public class Listener : BackgroundService
+    {
+        private readonly ILogger<Listener> _logger;
+        private readonly UdpClient _server;
+
+        public Listener(ILogger<Listener> logger, UdpClient server)
+        {
+            _logger = logger;
+            _server = server;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Listener running at: {time}", DateTimeOffset.Now);
+
+                
+                var result = await _server.ReceiveAsync();
+                //var message = Encoding.UTF8.GetString(result.Buffer);
+                //Console.WriteLine(message);
+                
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
