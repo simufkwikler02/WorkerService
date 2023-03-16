5a0ba24b73851afe00fd288ffac46f089483ff5c
Oleg Azemsha <bguproga@gmail.com>
2023-03-16 20:29:53 +0300

    add generator

--- WorkerService1/Program.cs
+++ WorkerService1/Program.cs
@@ -1,18 +1,18 @@
 using WorkerService1;
 using LbsLibrary;
 
-var point = new Point();
-var line = point.ToString();
-point.Parse(line);
-var serviceee = new LbsService();
-serviceee.ReadAndSave("D:\\out_257.csv");
-double lat;
-double lon;
-var lbs = new LBS(257, 2, 84, 55722);
-serviceee.TryGetLatLng(lbs, out lon, out lat);
-lbs = new LBS();
-lbs = serviceee.FindLbs(20, 50);
-serviceee.TryGetLatLng(lbs, out lon, out lat);
+//var point = new Point();
+//var line = point.ToString();
+//point.Parse(line);
+//var serviceee = new LbsService();
+//serviceee.ReadAndSave("D:\\out_257.csv");
+//double lat;
+//double lon;
+//var lbs = new LBS(257, 2, 84, 55722);
+//serviceee.TryGetLatLng(lbs, out lon, out lat);
+//lbs = new LBS();
+//lbs = serviceee.FindLbs(20, 50);
+//serviceee.TryGetLatLng(lbs, out lon, out lat);
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
--- WorkerService1/workers/UdpSender.cs
+++ WorkerService1/workers/UdpSender.cs
@@ -1,5 +1,10 @@
 using System.Text;
 using System.Net.Sockets;
+using Aspose.Gis.Geometries;
+using Aspose.Gis;
+using System.Text.RegularExpressions;
+using LbsLibrary;
+using System.Globalization;
 
 namespace WorkerService1
 {
@@ -7,6 +12,8 @@ namespace WorkerService1
     {
         private readonly ILogger<UdpSender> _logger;
         private readonly UdpClient _server;
+  
+        private readonly VectorLayer layer = Drivers.Gpx.OpenLayer(@"D:\Point.gpx");
 
         public UdpSender(ILogger<UdpSender> logger)
         {
@@ -16,14 +23,55 @@ namespace WorkerService1
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            string s = string.Empty;
+            foreach (var feature in this.layer)
+            {
+                // Проверка геометрии MultiLineString
+                if (feature.Geometry.GeometryType == GeometryType.MultiLineString)
+                {
+                    // Читать трек
+                    var lines = (MultiLineString)feature.Geometry;                   
+                    s = lines.AsText();
+                    
+                }
+            }
+            var regex = new Regex(@"-?\d+(.)\d+ -?\d+(.)\d+ -?\d+(.)\d+");
+            MatchCollection matches = regex.Matches(s);
+            var TestPoint = new List<LbsLibrary.Point>();
+            foreach (Match match in matches)
+            {
+                var line = match.Value.Split(' ');
+                var point = new LbsLibrary.Point();
+                if(!double.TryParse(line[0], NumberStyles.Float, CultureInfo.InvariantCulture, out double value))
+                {
+                    continue;
+                }
+                point.Lon = value;
+
+                if (!double.TryParse(line[0], NumberStyles.Float, CultureInfo.InvariantCulture, out value))
+                {
+                    continue;
+                }
+                point.Lat = value;
+                point.Sat = 5;
+                TestPoint.Add(point);
+            }
+
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                foreach (var point in TestPoint)
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    point.Date = DateTime.Now;
 
-                var message = DateTimeOffset.Now.ToString();
-                byte[] data = Encoding.UTF8.GetBytes(message);
+                    var message = point.ToString();
+                    byte[] data = Encoding.UTF8.GetBytes(message);
 
-                await _server.SendAsync(data, stoppingToken);
+                    await _server.SendAsync(data, stoppingToken);
+                    await Task.Delay(3000, stoppingToken);
+                }
+
                 await Task.Delay(1000, stoppingToken);
             }
         }
