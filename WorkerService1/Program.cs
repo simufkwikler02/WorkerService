2383eca50d9d6154cd3582899f67fbbe6d1ffa08
Oleg Azemsha <bguproga@gmail.com>
2023-03-29 12:53:22 +0300

    add WaitingForAppStartupService

--- WorkerService1/Program.cs
+++ WorkerService1/Program.cs
@@ -1,23 +1,10 @@
 using LbsLibrary;
+using WorkerService1;
 using WorkerService1.Workers;
 using WorkerService1.ServiceConfig;
 
-//var point = new Point();
-//var line = point.ToString();
-//point.Parse(line);
-//var serviceee = new LbsService();
-//serviceee.ReadAndSave("D:\\out_257.csv");
-//double lat;
-//double lon;
-//var lbs = new LBS(257, 2, 84, 55722);
-//serviceee.TryGetLatLng(lbs, out lon, out lat);
-//lbs = new LBS();
-//lbs = serviceee.FindLbs(20, 50);
-//serviceee.TryGetLatLng(lbs, out lon, out lat);
-
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
-
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
@@ -27,6 +14,7 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddHostedService<UdpSender>();
         services.AddHostedService<UdpReceiver>();
         services.AddSingleton<LbsService>();
+        services.AddSingleton<WaitingForAppStartupService>();
     })
     .Build();
 
---
+++ WorkerService1/WaitingForAppStartupService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace WorkerService1;
+
+public class WaitingForAppStartupService
+{
+    private readonly IHostApplicationLifetime _lifetime;
+
+    public WaitingForAppStartupService(IHostApplicationLifetime lifetime)
+    {
+        _lifetime = lifetime;
+    }
+
+    public async Task<bool> WaitForAppStartup(CancellationToken stoppingToken)
+    {
+        var startedSource = new TaskCompletionSource();
+        await using var reg1 = _lifetime.ApplicationStarted.Register(() => startedSource.SetResult());
+
+        var cancelledSource = new TaskCompletionSource();
+        await using var reg2 = stoppingToken.Register(() => cancelledSource.SetResult());
+
+        var completedTask = await Task.WhenAny(startedSource.Task, cancelledSource.Task);
+
+        return completedTask == startedSource.Task;
+    }
+}
